b3c0aa3c7b862b23cc8f9ccd6a26b8214b5f68fa
user2 <user2@KH_B>
2019-04-25 19:31:18 +0900

    맴버 전체 수정본

--- style_it/src/main/java/web/controller/MemberController.java
+++ style_it/src/main/java/web/controller/MemberController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,19 +21,21 @@ public class MemberController {
 	public void maingo() {
 	}
 	
+	// 맴버 가입
 	@RequestMapping(value = "/member/join", method = RequestMethod.POST)
 	public String join(Member member) {
 		
 		
-		memberService.insertMember(member);
+		memberService.memberInsert(member);
 		
 		return "redirect:/home";
 	}
 	
+	// 맴버 로그인
 	@RequestMapping(value = "/member/login", method = RequestMethod.POST)
 	public String login(Member member, HttpSession session) {
 		
-		boolean login = memberService.loginMember(member);
+		boolean login = memberService.memberLogin(member);
 		
 		if(login==true) {
 			session.setAttribute("login", login);
@@ -42,9 +45,30 @@ public class MemberController {
 		}
 	}
 	
+	// 맴버 로그아웃
 	@RequestMapping(value = "/member/logout", method = RequestMethod.GET)
 	public String logout(HttpSession session) {
 		session.invalidate();
+		
+		return "redirect:/home";
+	}
+	
+	// 맴버 리스트 보기
+	@RequestMapping (value = "/member/list", method = RequestMethod.GET)
+	public void LoginList(Model model) {
+			
+	}
+	
+	// 맴버 팔로우
+	@RequestMapping(value = "/member/follow", method = RequestMethod.POST)
+	public String followInsert(HttpSession session, Model model) {
+		
+		return null;
+	}
+	
+	// 맴버 스타일링 퀴즈 추가
+	@RequestMapping(value = "/member/stylingquiz", method = RequestMethod.GET)
+	public String stylingquizInsert(int qq_no) {
 		
 		return "redirect:/home";
 	}
--- style_it/src/main/java/web/service/impl/MemberServiceImpl.java
+++ style_it/src/main/java/web/service/impl/MemberServiceImpl.java
@@ -1,5 +1,7 @@
 package web.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,20 +14,68 @@ public class MemberServiceImpl implements MemberService{
 	@Autowired MemberDao memberDao;	
 	
 	@Override
-	public void insertMember(Member member) {
+	public void memberInsert (Member member) {
 		
-		memberDao.insertMember(member);
+		memberDao.memberInsert(member);
 	}
 
 	@Override
-	public boolean loginMember(Member member) {
+	public boolean memberLogin(Member member) {
 		
-		if(memberDao.loginMember(member)==1) {
+		if(memberDao.memberLogin(member)==1) {
 			return true;
 		}else {
 			return false;
 		}
 		
+		
+	}
+
+	@Override
+	public List<Member> getLoginList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Member> getLogoutList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void memberUpdate(int m_no) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void memberinfoUpdate(int m_no) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void followInsert(int f_no) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void followDelete(int f_no) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stylingquizInsert(int qq_no) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stylingquizUpdate(int qq_no) {
+		// TODO Auto-generated method stub
 		
 	}
 
