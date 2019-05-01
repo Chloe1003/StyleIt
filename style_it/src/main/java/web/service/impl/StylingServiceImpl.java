7a19d7ae9b57308afc04803420148b1d9b53e006
Chloe1003 <yea.s1003@gmail.com>
2019-05-01 20:27:34 +0900

    logger 추가, styling tag page 작업중

--- style_it/src/main/java/web/service/impl/StylingServiceImpl.java
+++ style_it/src/main/java/web/service/impl/StylingServiceImpl.java
@@ -8,54 +8,60 @@ import org.springframework.stereotype.Service;
 import web.dao.face.StylingDao;
 import web.dto.StylingComment;
 import web.dto.StylingLike;
+import web.dto.StylingTag;
 import web.service.face.StylingService;
 @Service
 public class StylingServiceImpl implements StylingService{
-	@Autowired StylingDao stylingDao;
+	@Autowired StylingDao sDao;
 	
 	@Override
 	public List getStylingList() {
-		return stylingDao.getStylingList();
+		return sDao.getStylingList();
 	}
 
 	@Override
 	public void getStylingView(int s_no) {
-		stylingDao.getStylingView(s_no);
+		sDao.getStylingView(s_no);
 	}
 
 	@Override
 	public void StylingLikeInsert(StylingLike sLike) {
-		stylingDao.StylingLikeInsert(sLike);
+		sDao.StylingLikeInsert(sLike);
 	}
 
 	@Override
 	public void StylingLikeDelete(StylingLike sLike) {
-		stylingDao.StylingLikeDelete(sLike);
+		sDao.StylingLikeDelete(sLike);
 	}
 
 	@Override
 	public void CollectionInsert(int cs_no) {
-		stylingDao.CollectionInsert(cs_no);
+		sDao.CollectionInsert(cs_no);
 	}
 
 	@Override
 	public void CollectionDelete(int cs_no) {
-		stylingDao.CollectionDelete(cs_no);
+		sDao.CollectionDelete(cs_no);
 	}
 
 	@Override
 	public void CommentInsert(int s_no) {
-		stylingDao.CommentInsert(s_no);
+		sDao.CommentInsert(s_no);
 	}
 
 	@Override
 	public void CommentDelete(int s_no) {
-		stylingDao.CommentDelete(s_no);
+		sDao.CommentDelete(s_no);
 	}
 
 	@Override
 	public void getStylingCommentList(StylingComment sComment) {
-		stylingDao.getStylingCommentList(sComment);
+		sDao.getStylingCommentList(sComment);
+	}
+
+	@Override
+	public List<StylingTag> getStylingTag() {
+		return sDao.selectStylingTag();
 	}
 
 }
